940bb0e2d2abe57f2df853a8bf8a92f23a9a7eb5
jordanarpad <jordan.arpad@gmail.com>
2022-10-07 20:03:58 +0100

    added TM1637 display

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import network
 import secrets
 import time
-import json
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
@@ -23,135 +22,59 @@ else:
     print( 'ip = ' + status[0] )
 
 from machine import Timer, Pin, PWM
-
-tim = Timer()
-alm = Timer()
-rem = Timer()
-led = Pin('LED', Pin.OUT)
-buzzer = PWM(Pin(15))
-buzzer.freq(500)
-
-def active(timer):
-    led.toggle()
-
-def expired(timer):
-    rem.init()
-    remain = 0
-    print('Published remain')
-    print(remain)
-    client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
-    tim.init(freq=3, mode=Timer.PERIODIC, callback=alarm)
-
-def alarm(timer):
-    led.toggle()
-    buzzer.duty_u16(1000)
-    time.sleep(0.1)
-    buzzer.duty_u16(0)
-
-def update(timer):
-    global remain
-    if remain > 0:
-        remain -= 1
-        print('Published remain')
-        print(remain)
-        client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
-    client.ping()
-    rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
-
+import tm1637
 from umqtt.simple import MQTTClient
-
-def mqtt_connect():
-    endpoint = secrets.ENDPOINT
-    port_no = 8883
-    cert_file = secrets.CERT_FILE
-    key_file = secrets.KEY_FILE
-    with open(cert_file, 'rb') as f:
-        cert = f.read()
-    with open(key_file, 'rb') as f:
-        key = f.read()
-    print('Certificate and Private Key loaded')
-    sslparams = {'cert':cert, 'key':key}
-    client = MQTTClient(client_id='picow', server=endpoint, port=port_no, keepalive=60, ssl=True, ssl_params=sslparams)
-    client.connect()
-    print('Connected to AWS IoT Core MQTT Broker')
-    led.value(1)
-    return client
-
-def callback_handler(topic, message_receive):
-    global remain
-    print('Received message')
-    minutes = int(message_receive.decode('UTF-8'))
-    print(minutes)
-    if minutes == 0:
-        tim.init()
-        alm.init()
-        remain = 0
-        print('Published remain')
-        print(remain)
-        client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
-        rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
-        led.value(1)
-    else:import network
-import secrets
-import time
 import json
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect(secrets.SSID, secrets.PASSWORD)
-
-max_wait = 10
-while max_wait > 0:
-    if wlan.status() < 0 or wlan.status() >= 3:
-        break
-    max_wait -= 1
-    print('waiting for connection...')
-    time.sleep(1)
-
-if wlan.status() != 3:
-    raise RuntimeError('network connection failed')
-else:
-    print('Wi-Fi connected')
-    status = wlan.ifconfig()
-    print( 'ip = ' + status[0] )
-
-from machine import Timer, Pin, PWM
-
 tim = Timer()
 alm = Timer()
 rem = Timer()
 led = Pin('LED', Pin.OUT)
-buzzer = PWM(Pin(15))
+buzzer = PWM(Pin(13))
 buzzer.freq(500)
+tm = tm1637.TM1637(clk=Pin(27), dio=Pin(28))
 
 def active(timer):
-    led.toggle()
+    global remain, sec
+    sec -= 1
+    if (sec % 2 == 0):
+        led.toggle()
+    if (sec % 3 == 0):
+        tm.numbers(remain-1, sec//3)
 
 def expired(timer):
+    global remain, sec
     rem.init()
     remain = 0
+    sec = 0
     print('Published remain')
     print(remain)
     client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
+    tm.numbers(remain, sec)
     tim.init(freq=3, mode=Timer.PERIODIC, callback=alarm)
 
 def alarm(timer):
+    global remain, sec
     led.toggle()
     buzzer.duty_u16(1000)
+    tm.write([0, 0, 0, 0])
     time.sleep(0.1)
+    tm.numbers(remain, sec)
     buzzer.duty_u16(0)
 
 def update(timer):
-    global remain
+    global remain, sec
     if remain > 0:
         remain -= 1
+        sec = 0
         print('Published remain')
         print(remain)
         client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
+        tm.numbers(remain, sec)
+        sec = 180
+        tim.init(freq=3, mode=Timer.PERIODIC, callback=active)
     client.ping()
     rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
-
-from umqtt.simple import MQTTClient
 
 def mqtt_connect():
     endpoint = secrets.ENDPOINT
@@ -167,11 +90,10 @@ def mqtt_connect():
     client = MQTTClient(client_id='picow', server=endpoint, port=port_no, keepalive=60, ssl=True, ssl_params=sslparams)
     client.connect()
     print('Connected to AWS IoT Core MQTT Broker')
-    led.value(1)
     return client
 
 def callback_handler(topic, message_receive):
-    global remain
+    global remain, sec
     print('Received message')
     minutes = int(message_receive.decode('UTF-8'))
     print(minutes)
@@ -179,17 +101,22 @@ def callback_handler(topic, message_receive):
         tim.init()
         alm.init()
         remain = 0
+        sec = 0
         print('Published remain')
         print(remain)
         client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
+        tm.numbers(remain, sec)
         rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
         led.value(1)
     else:
         remain = minutes
+        sec = 0
         print('Published remain')
         print(remain)
         client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
-        tim.init(freq=1.5, mode=Timer.PERIODIC, callback=active)
+        tm.numbers(remain, sec)
+        sec = 180
+        tim.init(freq=3, mode=Timer.PERIODIC, callback=active)
         alm.init(mode=Timer.ONE_SHOT, period=minutes*60000, callback=expired)
         rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
 
@@ -197,33 +124,13 @@ client = mqtt_connect()
 client.set_callback(callback_handler)
 client.subscribe(topic="picow")
 remain = 0
+sec = 0
 print('Published remain')
 print(remain)
 client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
+tm.numbers(remain, sec)
 rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
-
-
-while True:
-    try:
-        client.check_msg()
-    except OSError as e:
-        machine.reset()
-        remain = minutes
-        print('Published remain')
-        print(remain)
-        client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
-        tim.init(freq=1.5, mode=Timer.PERIODIC, callback=active)
-        alm.init(mode=Timer.ONE_SHOT, period=minutes*60000, callback=expired)
-        rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
-
-client = mqtt_connect()
-client.set_callback(callback_handler)
-client.subscribe(topic="picow")
-remain = 0
-print('Published remain')
-print(remain)
-client.publish(topic="remain", msg=json.dumps({"message":str(remain)}))
-rem.init(mode=Timer.ONE_SHOT, period=60000, callback=update)
+led.value(1)
 
 
 while True:
