4a60bfba04763ad95d24151da6e79e4acdf7599d
jordanarpad <jordan.arpad@gmail.com>
2022-10-16 08:30:24 +0100

    moving to pem files converted with ubinascii

--- secrets.py
+++ secrets.py
@@ -1,5 +1,5 @@
 SSID = "your_SSID"
 PASSWORD = "your_wifi_password"
 ENDPOINT = "your_iot_core_endpoint"
-CERT_FILE = "your_der_cert_file"
-KEY_FILE = "your_der_private_key_file"
+CERT_FILE = "your_pem_cert_file"
+KEY_FILE = "your_pem_private_key_file"
